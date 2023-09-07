b7a05d42ea5ade05fcf0f318a79c92c49a5f544c
welangaieric <welangaieric@gmail.com>
2023-09-07 13:30:22 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * function to prit out a string
+ */
+int main(void)
+{
+	char myString[120] = "Programming is like building a multilingual puzzle";
+	puts(myString);
+	return (0);
+}
