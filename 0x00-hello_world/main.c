db9be6efd280f8617ae07b241289e5576571c7c5
welangaieric <welangaieric@gmail.com>
2023-09-07 10:57:34 -0700

    changed the 0-preprocessor file to two lines

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return:Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	
-
-	return(0);
+    return (0);
 }
