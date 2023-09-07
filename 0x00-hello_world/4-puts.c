7192c86894578d80f96200ed2118fd6b78d163a7
welangaieric <welangaieric@gmail.com>
2023-09-07 13:42:02 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/*
- * function to prit out a string
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char myString[120] = "\"Programming is like building a multilingual puzzle";
+
 	puts(myString);
 	return (0);
 }
