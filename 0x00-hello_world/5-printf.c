f23eca366449ec443019e4f6b231495f0ddbd8a4
welangaieric <welangaieric@gmail.com>
2023-09-07 14:51:05 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char myString[120]="with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s",myString);	
+	return (0);
+}
