cc025ce62657ce77c268f7814aaaa50e3ee254e2
welangaieric <welangaieric@gmail.com>
2023-09-10 05:30:57 -0700

    9-print_comb.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar ('\n');
+	return (0);
+}
