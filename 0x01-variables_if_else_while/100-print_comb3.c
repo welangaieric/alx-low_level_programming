f7952c723d00eb172bf36b65ffec30a6a50095e0
welangaieric <welangaieric@gmail.com>
2023-09-10 08:08:49 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (n = 0; n < 9; n++)
 	{
-		for(m = n + 1; m < 10; m++)
+		for (m = n + 1; m < 10; m++)
 		{
 			putchar((n % 10) + '0');
 			putchar((m % 10) + '0');
 			if (n == 8 && m == 9)
 				continue;
 			putchar(',');
-			putchar(' ');  
+			putchar(' ');
 		}
 	}
 	putchar('\n');
