b79e8a3305e70613e3047622c7e39378fb628341
welangaieric <welangaieric@gmail.com>
2023-09-10 03:28:20 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets in lowercase then in uppercase
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char myChr;
+
+        for (myChr = 'a'; myChr <= 'z'; myChr++)
+                putchar(myChr);
+	for (myChr = 'A'; myChr <= 'Z'; myChr++)
+		putchar(myChr);
+        putchar ('\n');
+        return (0);
+}
