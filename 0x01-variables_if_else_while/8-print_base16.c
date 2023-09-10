b7e5b5af5c1082342f826ac0801fead48a61472a
welangaieric <welangaieric@gmail.com>
2023-09-10 05:15:44 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,11 @@
 int main(void)
 {
         int num;
+	char m;
 
         for (num = 0; num < 10; num++)
                 putchar((num % 10) + '0');
-        for (char m = 'a'; m <= 'f'; m++)
+        for (m = 'a'; m <= 'f'; m++)
                 putchar(m);
         putchar ('\n');
         return (0);
