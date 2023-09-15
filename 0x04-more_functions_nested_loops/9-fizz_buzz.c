289da043e23114f63beceed511397a12360cb3f2
welangaieric <welangaieric@gmail.com>
2023-09-14 19:45:45 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,14 +14,23 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-			printf(" fizz");
-		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
 			printf(" Buzz");
-		else if (i % 3 == 0 && i % 5 ==0)
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf(" FizzBuzz");
-		else
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
 			printf(" %d", i);
+		}
 	}
 	printf("\n");
+
 	return (0);
 }
