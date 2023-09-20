380099e47caa3d408baa94bc452ed8232ab6c96e
welangaieric <welangaieric@gmail.com>
2023-09-19 19:47:30 -0700

    pointers , arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-void *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 #endif
 
