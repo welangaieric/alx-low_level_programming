6d26508ad12f17f179ed1b76e0925279e2fc17ff
welangaieric <welangaieric@gmail.com>
2023-10-10 16:14:42 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - this is an animal
+ *@name : name of the dog
+ *@age : age of the dog
+ *@owner : the owner of the dog
+ */
+
 struct dog
 {
 	char *name;
