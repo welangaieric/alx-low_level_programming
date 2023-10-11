b702e695aaacb53e63dbcf9192f9ee1ddd9c0f8b
welangaieric <welangaieric@gmail.com>
2023-10-10 17:24:26 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog
+{
+	char *name;
+        float age;
+        char *owner;
+}dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
