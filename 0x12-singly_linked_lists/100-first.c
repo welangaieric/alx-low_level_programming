9639752a5874e192d403360679048674e0a5a7b2
welangaieric <welangaieric@gmail.com>
2023-10-20 14:50:02 -0700

    added tasks in the linked lists project

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,19 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-/**
-* myStartupFun - Apply the constructor attribute to myStartupFun() so that it
-*   is executed before main()
-*/
-void myStartupFun(void) __attribute__((constructor));
-
+void first(void) __attribute__ ((constructor));
 
 /**
-* myStartupFun - implementation of myStartupFun
-*/
-void myStartupFun(void)
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
 {
-   printf("You're beat! and yet, you must allow,\n");
-   printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
-
