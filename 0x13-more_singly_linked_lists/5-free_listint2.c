1d0349e9a7f2068657d04e71fb91df03ca6d0942
welangaieric <welangaieric@gmail.com>
2023-10-24 15:52:28 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,16 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *inf;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = (*head)->next;
+		inf  = (*head)->next;
 		free(*head);
-		*head = temp;
+		*head = inf;
 	}
 
 	*head = NULL;
